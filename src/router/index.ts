9169f9387f8bccbaef4dacfb8d86390a3890df7a
testusuario <pruebamorafoo@gmail.com>
2022-06-27 12:20:44 -0700

    se cambian nombres de rutas para netlify

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,8 +18,8 @@ const router = createRouter({
       component: () => import('../views/RegistroView.vue')
     },
     {
-      path: '/iniciar-sesion',
-      name: 'iniciar-sesion',
+      path: '/iniciarsesion',
+      name: 'iniciarsesion',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
